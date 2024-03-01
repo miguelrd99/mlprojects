8c6570f5bcfc3056eab77273e7891a495b820957
miguelrd99 <Mi@MacBook-Air-de-Miguel.local>
2024-03-01 14:10:04 +0100

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -14,5 +14,7 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message, error_detail=error_detail)
 
-    def __Str__(self):
+    def __str__(self):
         return self.error_message
+
+        
